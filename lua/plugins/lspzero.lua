86b45831b3f7ef5be6b470d3bd602772d165af01
Ruslan Musaev <rm@Ruslans-Mac-mini.local>
2025-02-24 21:49:41 -0300

    -

--- lua/plugins/lspzero.lua
+++ lua/plugins/lspzero.lua
@@ -41,7 +41,7 @@ return {
       }
 
     })
-    require('lspconfig').tsserver.setup({})
+    require('lspconfig').ts_ls.setup({})
 
     ---
     -- Autocompletion setup
--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,5 +1,3 @@
-update treesitter:
-
 return {"nvim-treesitter/nvim-treesitter", build = ":TSUpdate", config = function () 
 	local configs = require("nvim-treesitter.configs")
 
@@ -9,34 +7,34 @@ return {"nvim-treesitter/nvim-treesitter", build = ":TSUpdate", config = functio
 		highlight = { enable = true },
 		indent = { enable = true },  
 	})
+
+  -- add type displaying keybinging
+  vim.keymap.set("n", "<leader>t", vim.lsp.buf.hover, { desc = "Show type of symbol under cursor" })
+
+  -- update diagnostics (errors) display:
+  vim.diagnostic.config({
+    virtual_text = false, -- Turn off inline diagnostics
+  })
+
+  -- Show all diagnostics on current line in floating window
+  vim.api.nvim_set_keymap(
+    'n', '<Leader>d', ':lua vim.diagnostic.open_float()<CR>', 
+    { noremap = true, silent = true }
+  )
+
+  -- Go to next diagnostic (if there are multiple on the same line, only shows
+  -- one at a time in the floating window)
+  vim.api.nvim_set_keymap(
+    'n', '<Leader>n', ':lua vim.diagnostic.goto_next()<CR>',
+    { noremap = true, silent = true }
+  )
+
+  -- Go to prev diagnostic (if there are multiple on the same line, only shows
+  -- one at a time in the floating window)
+  vim.api.nvim_set_keymap(
+    'n', '<Leader>p', ':lua vim.diagnostic.goto_prev()<CR>',
+    { noremap = true, silent = true }
+  )
 end}
-
--- add type displaying keybinging
-vim.keymap.set("n", "<leader>t", vim.lsp.buf.hover, { desc = "Show type of symbol under cursor" })
-
-
--- update diagnostics (errors) display:
-
-vim.diagnostic.config({
-  virtual_text = false, -- Turn off inline diagnostics
-})
-
--- Show all diagnostics on current line in floating window
-vim.api.nvim_set_keymap(
-  'n', '<Leader>d', ':lua vim.diagnostic.open_float()<CR>', 
-  { noremap = true, silent = true }
-)
--- Go to next diagnostic (if there are multiple on the same line, only shows
--- one at a time in the floating window)
-vim.api.nvim_set_keymap(
-  'n', '<Leader>n', ':lua vim.diagnostic.goto_next()<CR>',
-  { noremap = true, silent = true }
-)
--- Go to prev diagnostic (if there are multiple on the same line, only shows
--- one at a time in the floating window)
-vim.api.nvim_set_keymap(
-  'n', '<Leader>p', ':lua vim.diagnostic.goto_prev()<CR>',
-  { noremap = true, silent = true }
-)
 
 
