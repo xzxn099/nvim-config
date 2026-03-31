caecdd4128f873dd04e063906dc166b53ff8be9a
Ruslan Musaev <rm@Ruslans-Mac-mini.local>
2026-03-31 16:19:23 -0300

    -

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -1,15 +1,35 @@
 return {
-    "0xstepit/flow.nvim",
-    lazy = false,
-    priority = 1000,
-    opts = {theme = {transparent = true}, colors = {fluo = "cyan"}, ui = {borders = "none", aggressive_spell = false}},
-    config = function(_, opts)
-        require("flow").setup(opts)
-        vim.cmd("colorscheme flow")
-        vim.api.nvim_set_hl(0, "Normal", {bg = "none"})
-        vim.api.nvim_set_hl(0, "NormalNC", {bg = "none"})
-        vim.api.nvim_set_hl(0, "NormalFloat", {bg = "none"})
-        vim.api.nvim_set_hl(0, "FloatBorder", {bg = "none", fg = "#27272a"})
-    end
-}
+  "0xstepit/flow.nvim",
+  lazy = false,
+  priority = 1000,
+  opts = {
+    theme = { transparent = true },
+    colors = { fluo = "cyan" },
+    ui = {
+      borders = "none",
+      aggressive_spell = false,
+    },
+  },
+  config = function(_, opts)
+    require("flow").setup(opts)
+    vim.cmd("colorscheme flow")
 
+    -- editor
+    vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+    vim.api.nvim_set_hl(0, "NormalNC", { bg = "none" })
+
+    -- regular floating windows like diagnostics / hover
+    vim.api.nvim_set_hl(0, "NormalFloat", { bg = "#18181b" })
+    vim.api.nvim_set_hl(0, "FloatBorder", { bg = "#18181b", fg = "#27272a" })
+
+    -- float titles / labels
+    vim.api.nvim_set_hl(0, "FloatTitle", { bg = "none" })
+    vim.api.nvim_set_hl(0, "FloatFooter", { bg = "none" })
+
+    -- mini.files specific
+    vim.api.nvim_set_hl(0, "MiniFilesNormal", { bg = "#000000" }) -- or "none"
+    vim.api.nvim_set_hl(0, "MiniFilesBorder", { bg = "#000000", fg = "#27272a" })
+    vim.api.nvim_set_hl(0, "MiniFilesTitle", { bg = "none" })
+    vim.api.nvim_set_hl(0, "MiniFilesTitleFocused", { bg = "none" })
+  end,
+}
