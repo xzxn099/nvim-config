abf102b26d48a6a472b55d206c757cf5334ff183
Ruslan Musaev <rm@Ruslans-Mac-mini.local>
2026-03-31 16:05:28 -0300

    wip: some plugin fixes

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -1,22 +1,15 @@
 return {
-	"0xstepit/flow.nvim",
-	lazy = false,
-	priority = 1000,
-	opts = {
-    theme = {
-      transparent = true, 
-    },
-    colors = {
-      fluo = "cyan", 
-    },
-    ui = {
-      borders = "theme", -- "theme" | "inverse" | "fluo" | "none"
-      aggressive_spell = false, -- true | false
-    },
-  },
-  config = function(_, opts)
-    require("flow").setup(opts)
-    vim.cmd("colorscheme flow")
-  end,
+    "0xstepit/flow.nvim",
+    lazy = false,
+    priority = 1000,
+    opts = {theme = {transparent = true}, colors = {fluo = "cyan"}, ui = {borders = "none", aggressive_spell = false}},
+    config = function(_, opts)
+        require("flow").setup(opts)
+        vim.cmd("colorscheme flow")
+        vim.api.nvim_set_hl(0, "Normal", {bg = "none"})
+        vim.api.nvim_set_hl(0, "NormalNC", {bg = "none"})
+        vim.api.nvim_set_hl(0, "NormalFloat", {bg = "none"})
+        vim.api.nvim_set_hl(0, "FloatBorder", {bg = "none", fg = "#27272a"})
+    end
 }
 
--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,40 +1,35 @@
-return {"nvim-treesitter/nvim-treesitter", build = ":TSUpdate", config = function () 
-	local configs = require("nvim-treesitter.configs")
+return {
+  "nvim-treesitter/nvim-treesitter",
+  lazy = false,
+  build = ":TSUpdate",
+  config = function()
+    require("nvim-treesitter").setup({
+      ensure_installed = {
+        "lua",
+        "query",
+        "javascript",
+        "html",
+        "css",
+        "typescript",
+        "rust",
+        "sql",
+        "markdown",
+        "markdown_inline",
+        "swift",
+      },
+      sync_install = false,
+      highlight = { enable = true },
+      indent = { enable = true },
+    })
 
-	configs.setup({
-		ensure_installed = {"lua", "query", "javascript", "html", "css", "typescript", "rust", "sql", "markdown", "markdown_inline", "swift"},
-		sync_install = false,
-		highlight = { enable = true },
-		indent = { enable = true },  
-	})
+    vim.keymap.set("n", "<leader>t", vim.lsp.buf.hover, { desc = "Show type of symbol under cursor" })
 
-  -- add type displaying keybinging
-  vim.keymap.set("n", "<leader>t", vim.lsp.buf.hover, { desc = "Show type of symbol under cursor" })
+    vim.diagnostic.config({
+      virtual_text = false,
+    })
 
-  -- update diagnostics (errors) display:
-  vim.diagnostic.config({
-    virtual_text = false, -- Turn off inline diagnostics
-  })
-
-  -- Show all diagnostics on current line in floating window
-  vim.api.nvim_set_keymap(
-    'n', '<Leader>d', ':lua vim.diagnostic.open_float()<CR>', 
-    { noremap = true, silent = true }
-  )
-
-  -- Go to next diagnostic (if there are multiple on the same line, only shows
-  -- one at a time in the floating window)
-  vim.api.nvim_set_keymap(
-    'n', '<Leader>n', ':lua vim.diagnostic.goto_next()<CR>',
-    { noremap = true, silent = true }
-  )
-
-  -- Go to prev diagnostic (if there are multiple on the same line, only shows
-  -- one at a time in the floating window)
-  vim.api.nvim_set_keymap(
-    'n', '<Leader>p', ':lua vim.diagnostic.goto_prev()<CR>',
-    { noremap = true, silent = true }
-  )
-end}
-
-
+    vim.keymap.set("n", "<leader>d", vim.diagnostic.open_float, { desc = "Line diagnostics" })
+    vim.keymap.set("n", "<leader>n", vim.diagnostic.goto_next, { desc = "Next diagnostic" })
+    vim.keymap.set("n", "<leader>p", vim.diagnostic.goto_prev, { desc = "Previous diagnostic" })
+  end,
+}
